37a49f7a99fdb4b08972027dbcdc0eb2b3911084
ILHAM NADHIR <noorulilham3@gmail.com>
2026-06-20 14:20:58 +0530

    editor working

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useRef } from 'react'
 import Editor from '@monaco-editor/react'
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import { MonacoBinding } from 'y-monaco'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const editorRef = useRef(null)
+
+  function handleEditorDidMount(editor) {
+    editorRef.current = editor
+    const doc = new Y.Doc()
+    const provider = new WebrtcProvider('test-room', doc)
+    const type = doc.getText('monaco')
+    new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
+    console.log(provider.awareness)
+  }
 
   return (
     <Editor
-    height="100vh"
-    width="100vw"
-    theme="vs-dark"
+      height="100vh"
+      width="100vw"
+      theme="vs-dark"
+      onMount={handleEditorDidMount}
     />
   )
 }
